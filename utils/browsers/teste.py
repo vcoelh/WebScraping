585806c32384a3481a6cd73f891bd2986417518a
Victor Coelho <coelhovictor0715@gmail.com>
2023-11-16 21:15:43 -0300

    deleting teste.ipynb and teste.py

--- utils/browsers/teste.py
+++
@@ -1,7 +0,0 @@
-from time import sleep
-
-from BrowserFactory import BrowserFactory
-driver = BrowserFactory()(webdriver='Chrome', arguments=['--start-maximized'])
-driver.get('https://curso.mairovergara.com/login')
-
-sleep(10)
--- utils/teste.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-dic = {
-    'data' : '20/07/2002'
-}
-
-df = pd.DataFrame(dic)
